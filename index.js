70df4201dea0d898c72e98a405b37cce3fd16f8d
Ryan Van Valkenhoef <ryanvanvalkenhoef@iMac-van-Ryan.local>
2024-03-11 23:18:04 +0100

    EXP-4 Change data of fetch-request

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const playwright = require("playwright")
-(async() => {
-for (const browserType of ['chromium', 'firefox',  'webkit']){
-   const browser = await playwright[browserType].launch()
-   const context = await browser.newContext()
-   const page = await context.newPage()
-   await page.goto("https://amazon.com")
-   await page.wait_for_timeout(1000)
+const playwright = require('playwright');
+const fs = require('fs');
+const https = require('https');
+
+
+module.exports = {
+    runBrowser: (async () => {
+
+        const browser = await playwright.chromium.launch({ headless: true });
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto('https://www.rijksoverheid.nl');
+        const products = await page.$$eval('div.brick', all_bricks => {
+            const data = [];
+            all_bricks.forEach(brick => {
+                const title = brick.querySelector('h3')?.textContent;
+                const caption = brick.querySelector('.brick-text p')?.textContent;
+                data.push({ title, caption })
+            });
+       return data;
+   })
+   console.log(products)
    await browser.close()
-   }
-})
+    })
+};
+
+module.exports.runBrowser();
