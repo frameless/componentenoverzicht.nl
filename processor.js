981f6efacd2b84e918aa66c695c14cd12cc45e24
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2024-03-21 18:24:05 +0100

    refactor: Restructured code and resolved crawling in processResults

--- processor.js
+++ processor.js
@@ -4,72 +4,84 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const processor = function processResults(results) {
     let libraryVersions = {};
-    let importedLibrary;
-    let output = {};
 
-    outer: for (const result in results['report']) {
-        let nldsUsed = false;
-        const component = results['report'][result];
-
-        // Only catch data when there's a moduleName (and it's not a html-element)
-        if (component.instances[0].importInfo) {
-            importedLibrary = component.instances[0].importInfo?.moduleName;
-
-            const stream = fs.createReadStream('./nlds-dataset.json', { encoding: 'utf8' }); {
-                stream.on('data', chunk => {
-                    Object.values(JSON.parse(chunk)['package-names']).filter((value) => {
-                        // Validate if the values of the dataset contain the source-library of the component
-                        if (importedLibrary.includes(value)) {
-                            nldsUsed = true;
-                        }
-                    })
-                });
-                stream.on('error', err => { console.error(`Fout bij het lezen van ./nlds-dataset.json: ${err}`); });
-                stream.on('end', () => {
-                    // Accumulate output
-                    output[result] = output[result] || {
-                        instancesCount: 0,
-                        id: uuidv4(),
-                        instances: [],
-                        nldsUsed: nldsUsed,
-                        isWebComponent: (component.componentType === "JSXOpeningElement" && importedLibrary.includes("web-component")),
-                        isCSSComponent: (component.componentType === "JSXOpeningElement" && importedLibrary.includes("component-library-css")
-                            || importedLibrary.includes("css-component"))
-                    };
-
-                    for (const i in component.instances) {
-                        const propEntries = Object.entries(component.instances[i].props);
-                        const instanceId = uuidv4();
-                        const instanceData = {
-                            id: instanceId,
-                            path: component.instances[i].location.file,
-                            projectName: "hebik.nldesignsystem.nl",
-                            // type: result.type,
-                            props: Object.fromEntries(propEntries),
-                        }
-                        output[result].instancesCount++;
-                        output[result].instances.push(instanceData);
-                    }
-                });
-            }
-        } else {
-            console.log(`Cannot find importInfo of ${component.instances[0]}`)
-        }
+    let json, jsonObject;
+    try {
+        json = fs.readFileSync('./nlds-dataset.json', { encoding: 'utf8' });
+        jsonObject = JSON.parse(json);
+    } catch (e) {
+        console.error(`Fout bij het lezen van ./nlds-dataset.json: ${e.error}`);
     }
+
+    Object.values(results['report'])
+        .filter((component) => !component.instances[0].importInfo)
+        .forEach((component) => {
+            console.log(`Cannot find importInfo of ${component.instances[0]}`)
+        });
+
+    const output = Object.entries(results['report'])
+        .filter(([result, component]) => component.instances[0].importInfo)
+        .reduce((output, [componentName, component]) => {
+
+            const importedLibrary = component.instances[0].importInfo?.moduleName;
+
+            const nldsUsed = Object.values(jsonObject['package-names']).some((value) => {
+                // Validate if the values of the dataset contain the source-library of the component
+                return importedLibrary.includes(value);
+            });
+
+            const instances = Object.values(component.instances).map((instance) => {
+                const propEntries = Object.entries(instance.props);
+                const instanceId = uuidv4();
+                return {
+                    id: instanceId,
+                    path: instance.location.file,
+                    projectName: "hebik.nldesignsystem.nl",
+                    type: component.type,
+                    props: Object.fromEntries(propEntries),
+                }
+                // output[componentName].instancesCount++;
+                // output[componentName].instances.push(instanceData);
+            });
+
+            const instancesCount = (output[componentName] ? output[componentName].instancesCount : 0) + instances.length;
+            // Accumulate output
+            return {
+                ...output,
+                [componentName]: output[componentName] || {
+                    importedLibrary,
+                    instancesCount,
+                    id: uuidv4(),
+                    instances: [...(output[componentName] ? output[componentName].instances : []), ...instances],
+                    nldsUsed: nldsUsed,
+                    isWebComponent: (component.componentType === "JSXOpeningElement" && importedLibrary.includes("web-component")),
+                    isCSSComponent: (component.componentType === "JSXOpeningElement" && importedLibrary.includes("component-library-css")
+                        || importedLibrary.includes("css-component"))
+                }
+            };
+        }, {});
+
     // Catch NLDS libraries and versions from all package.json files
     const files = findPackageJsonFiles('./temp');
     for (const file in files) {
-        const stream = fs.createReadStream(files[file], { encoding: 'utf8' }); {
-            stream.on('data', chunk => {
-                if (importedLibrary.includes('web-component') || importedLibrary.includes('component-library-css' || importedLibrary.includes('css-component'))) {
-                    const library = importedLibrary.split('/')[0];
-                    console.log('library: ' + library);
-                    libraryVersions[library] = JSON.parse(chunk)['devDependencies'][library];
+        let json, jsonObject;
+        try {
+            json = fs.readFileSync(files[file], { encoding: 'utf8' });
+            jsonObject = JSON.parse(json);
+            inner: for (const componentName in output) {
+                const library = output[componentName].importedLibrary;
+                if (library.includes('web-component') || library.includes('component-library-css' || library.includes('css-component'))) {
+                    const nodePackage = (library.split('/')[0] != "@") ? library.split('/')[0] : null;
+                    if (nodePackage) {
+                        Object.entries(jsonObject['devDependencies']).forEach(([key, value]) => {
+                            if (library.includes(key)) libraryVersions[library] = value;
+                        });
+                    }
                 }
-            });
+            }
+        } catch (e) {
+            // console.error(`Fout bij het lezen van ${files[file]}: ${e.error}`);
         }
-        stream.on('error', err => { console.error(`Fout bij het lezen van ${files[file]}: ${err}`); });
-        stream.on('end', () => { });
     }
 
     return { output: output, versions: libraryVersions };
